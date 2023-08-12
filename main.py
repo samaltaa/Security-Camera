20536117c6f744f60ac05fd574ac83ebc53c10b1
Samira <samirasanchez8090@gmail.com>
2023-08-12 12:40:03 -0400

    calculate eye gaze direction

--- main.py
+++ main.py
@@ -66,8 +66,16 @@ while(True):
          eyes = eyeCascade.detectMultiScale(roi_gray)
          for (ex,ey,ew,eh) in eyes:
               cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-    
+              #calculate the eye gaze direction
+              eye_roi_gray = roi_gray[ey:ey+eh, ex:ex+ew]
+              eye_roi_color = roi_color[ey:ey+eh, ex:ex+ew]
+              _, eye_thresh = cv2.threshold(eye_roi_gray, 50, 255, cv2.THRESH_BINARY_INV)
+              contours, _ = cv2.findContours(eye_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+              if len(contours) > 0:
+                   pupil = max(contours, key=cv2.contourArea)
+                   x1, y1, w1, h1 = cv2.boundingRect(pupil)
+                   center = (int(x1 + w1/2), int(y1 + h1/2))
+                   cv2.circle(eye_roi_color, center, 3, (255, 0, 0), -1)
 
     # draw a rectangle around the faces detected 
     for (x, y, w, h) in faces:
@@ -75,7 +83,7 @@ while(True):
 
 
     #display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('Security Cam', frame)
 
     # the 'q' button is set as the quitting button 
     if cv2.waitKey(1) & 0xFF == ord('q'):
